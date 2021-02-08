d1251ccd781b7c30bcf5a7b872551f747547ab46
Claes Östlund <claes@stenbergit.se>
2021-02-08 14:28:01 +0100

    1.1.0

--- PluginWfArraysearch.php
+++ PluginWfArraysearch.php
@@ -1,15 +1,4 @@
 <?php
-/**
- * Search in array. 
- * Get all keys or get all keys by name and/or by value.
- * 
-    wfPlugin::includeonce('wf/arraysearch');
-    $search = new PluginWfArraysearch(true);
-    $search->data = array('key_name' => 'three', 'key_value' => '3', 'data' => array('one' => '1', 'two' => array('three' => 3, 'four' => '4'), 'three' => 33));
-    wfHelp::yml_dump($search->get());    
-    Return array: - /two/three
- * 
- */
 class PluginWfArraysearch{
   private $keys = array();
   public $data = array();
@@ -26,6 +15,15 @@ class PluginWfArraysearch{
   private function getKeys($arr, $keys = null){
     if(is_array($arr)){
       foreach ($arr as $key => $value) {
+        /**
+         * 
+         */
+        if(!strlen($key)){
+          continue;
+        }
+        /**
+         * 
+         */
         $log_key = false;
         if(!is_array($value)){
           if(!strlen($this->data['key_name']) && !strlen($this->data['key_value'])){
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Buto-Plugin-WfArraysearch
 Search in array.
+## Usage 
+Get all keys or get all keys by name and/or by value.
+```
+wfPlugin::includeonce('wf/arraysearch');
+$search = new PluginWfArraysearch(true);
+$search->data = array('key_name' => 'three', 'key_value' => '3', 'data' => array('one' => '1', 'two' => array('three' => 3, 'four' => '4'), 'three' => 33));
+wfHelp::yml_dump($search->get());    
+Return array: - /two/three
+```
